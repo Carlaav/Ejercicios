b6e9f380c24e82d137968f5b281dce1fbf42f30b
Carlaav <carla2002.av@gmail.com>
2022-03-08 16:51:44 +0100

    a

--- 3.py
+++ 3.py
@@ -1,1 +1,4 @@
-
+#1 es herencia porque saturno estaria dentro de la clase planeta al ser un caso especifico de esta
+#2 no es herencia, no tiene pinta
+#3 es herencia porque perro estaria dentro de la clase animal de compañia al ser un caso especifico de esta
+#4 no es herencia porque es un atributo de la clase bicicleta
