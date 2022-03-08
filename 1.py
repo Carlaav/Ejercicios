248d6e9067aeac9f12145dbebe7af01c2d23c8e7
Carlaav <carla2002.av@gmail.com>
2022-03-08 14:41:05 +0100

    a

--- 1.py
+++ 1.py
@@ -1,1 +1,10 @@
+class clase:
+    def __init__(self, lista):
+        self.lista= list(lista)
 
+    def longitud(self):
+        return len(self.lista)
+a = [1, 3, 5, 3]
+
+z = clase(a)
+print(z.longitud())
