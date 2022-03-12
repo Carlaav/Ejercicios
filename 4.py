e837fabf53fd2b45aacd7dad82b7749c423792a2
Carlaav <carla2002.av@gmail.com>
2022-03-12 17:00:15 +0100

    a

--- 4.py
+++ 4.py
@@ -1,21 +1,31 @@
+from enum import Enum
+
+
 class cuidadores:       
-    def __init__(self):
-        self.cuidadores= ["juan","mario","pau"]
+    def __init__(self,nombre,apellido,animales):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.animales=animales
+
 class comida:
       pass
-class herviboro(comida):
-      pass
-class insectivoro(comida):
-      pass
 class carnivoro(comida):
-      pass
-
-        
-
+      def __init__(self,tipo):
+        self.carnivoro=tipo
+class herbivoro(comida):
+       def __init__(self,tipo):
+            self.herbivoro=tipo
+class insectivoro(comida):
+       def __init__(self,tipo):
+            self.insectivoro=tipo      
 class animales:
-    def __init__(self):
-        self.id= "id"
-        self.comida=comida()
-        self.cuidador= cuidadores()
+    def __init__(self,raza,tipo_comida,numero):
+        self.id= numero 
+        self.comida=comida(tipo_comida)
+        self.raza= raza
 class stock:
       pass
+class dieta(Enum):
+      carnivoro=carnivoro()
+      herviboro=herbivoro()
+      insectivoro=insectivoro()
