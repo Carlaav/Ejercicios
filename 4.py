3bd100c7f75279e8674f8e65c954e3a1de018ecf
Carlaav <carla2002.av@gmail.com>
2022-03-12 16:23:58 +0100

    a

--- 4.py
+++ 4.py
@@ -1,1 +1,21 @@
+class cuidadores:       
+    def __init__(self):
+        self.cuidadores= ["juan","mario","pau"]
+class comida:
+      pass
+class herviboro(comida):
+      pass
+class insectivoro(comida):
+      pass
+class carnivoro(comida):
+      pass
 
+        
+
+class animales:
+    def __init__(self):
+        self.id= "id"
+        self.comida=comida()
+        self.cuidador= cuidadores()
+class stock:
+      pass
