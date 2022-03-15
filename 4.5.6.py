a7600f94be5fbe5a9bf4caa3ab070d8c6342a4ca
dmunofer <diego.fmz@hotmail.com>
2022-03-15 21:43:00 +0100

    4.5.6 mejorado

--- 4.5.6.py
+++ 4.5.6.py
@@ -2,40 +2,66 @@ from enum import Enum
 class zoo:
       cuidadores=[]
       animales=[]
-      stock=stock
+      stock={}
+
+      @staticmethod
+      def asignar_cuidador(cuidador,animal):
+            cuidador = cuidadores.__init__(cuidador)
+            animal = animales.__init__(animal)
+            animal.encargado= cuidador
+
+      @staticmethod
+      def dar_vacaciones(solicitud):
+            if solicitud:
+                  cuidadores.descanso=solicitud
+            else:
+                  pass
+
 class vacaciones:
       def __init__(self,inicio,duracion,empleado):
             self.vacacionero=empleado
             self.inicio=inicio
             self.duracion=duracion
-class cuidadores:       
-    def __init__(self,nombre,apellido,animales,inicio,duracion):
-        self.nombre=nombre
-        self.apellido=apellido
-        self.animales=animales
-        self.descanso=vacaciones(inicio,duracion,self)
+class cuidadores:
+      def __init__(self,nombre,apellido,animales,inicio,duracion):
+            self.nombre=nombre
+            self.apellido=apellido
+            self.animales=animales
+            self.descanso=vacaciones(inicio,duracion,vacaciones.vacacionero)
+
+      @staticmethod
+      def solicitar_vacaciones(inicio,duracion,empleado):
+            solicitud = (inicio,duracion,empleado)
+            return solicitud
+
 class stock:
       suministros={}
+
+      def esta_vacio(self, suministros):
+            if suministros:
+                  pass
+            else:
+                  raise Exception("No hay stock")
 class comida:
       food=[]
 class carnivoro:
       def __init__(self):
-        self.tipo="carnivoro"
+            self.tipo="carnivoro"
 class herbivoro:
-       def __init__(self):
+      def __init__(self):
             self.tipo="herbivoro"
 class insectivoro:
-       def __init__(self):
-            self.tipo="insectivoro" 
+      def __init__(self):
+            self.tipo="insectivoro"
 class dieta(Enum):
-      carnivoro=carnivoro.tipo()
-      herviboro=herbivoro.tipo()
-      insectivoro=insectivoro.tipo()   
+      carnivoro=carnivoro.__init__(Enum)
+      herviboro=herbivoro.__init__(Enum)
+      insectivoro=insectivoro.__init__(Enum)
 class animales:
-    def __init__(self,especie,tipo_comida,numero,cuidador):
-        self.id= numero 
-        self.especie= especie
-        self.alimentacion=dieta[tipo_comida].name
-        self.encargado=cuidador
+      def __init__(self,especie,tipo_comida,numero,cuidador):
+            self.id= numero
+            self.especie= especie
+            self.alimentacion=dieta[tipo_comida].name
+            self.encargado=cuidador
 
-      
+
