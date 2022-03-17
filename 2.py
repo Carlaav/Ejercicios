26f206bfc73a303cc0b5edd5b3d77a53a10a3a7e
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2022-03-17 13:39:10 +0100

    enlace

--- 2.py
+++ 2.py
@@ -1,5 +1,5 @@
 #1 Es agregación porque un aeropuerto puede no tener aviones y seguira siendo el edificio aeropuerto y un avión no necesita un aeropuerto para existir
 #2 Composición porque todos los paises pertenecen a un continente, no puede haber un pais sin continente
 #3 Composición porque una molecula es un conjunto de átomos
-#4 Composición no puede haber una colmena sin abejas y las avejas forman una colmena, sin ellas no puede haberla
+#4 Composición no puede haber una colmena sin abejas y las abejas forman una colmena, sin ellas no puede haberla
 #5 Agregación, una muñeca puede existir independientemente a la otra
